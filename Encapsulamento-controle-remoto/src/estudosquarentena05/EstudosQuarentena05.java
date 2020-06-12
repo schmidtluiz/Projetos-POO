f16a6c28c809f79584ad6e1b30d1fd03b622d109
schmidtluiz <51960129+schmidtluiz@users.noreply.github.com>
2020-06-12 16:05:03 -0300

    commit encapsulamento

---
+++ Encapsulamento-controle-remoto/src/estudosquarentena05/EstudosQuarentena05.java
@@ -0,0 +1,16 @@
+
+package estudosquarentena05;
+
+public class EstudosQuarentena05 {
+
+    public static void main(String[] args) {
+        ControleRemoto c1=new ControleRemoto();
+        c1.ligar();
+        c1.maisVolume();
+        c1.ligarMudo();
+        c1.delisgarMudo();
+        c1.maisVolume();
+        c1.abrirMenu();
+    }
+    
+}
